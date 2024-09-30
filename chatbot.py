05348cf634da2a6223cf1b587a605cc0c53908b0
AntonRyans <antonryans17@gmail.com>
2024-09-30 17:55:15 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -16,28 +16,18 @@ age=int(input("What's your age? "))
 engine.say("Where are you from? ")
 engine.runAndWait()
 home=input("Where are you from? ")
+engine.say("Oh ," + str(home) + " is a nice place.")
+engine.runAndWait()
+print("Oh ," + home + " is a nice place.")
 engine.say("What do you study? ")
 engine.runAndWait()
 course=input("What do you study? ")
-myCon = mc.connector.connect( host=hostname, user=username, passwd=root, db = bot_data)
+myCon = mc.connect( host="localhost", user= "root", passwd="root", db = "bot_data")
 mycursor = myCon.cursor()
 sql = "INSERT INTO user_data (name, age, home, course) VALUES (%s, %s, %s, %s)"
 val = (name, age, home, course)
 mycursor.execute(sql, val)
 myCon.commit()
 print(mycursor.rowcount, "record inserted.")
-"""engine.say("Do you want to save your data to the database? ")
-engine.runAndWait()
-consent=input("Do you want to save your data to the database?[y/n] ")
-while consent.lower == "y":
-    myCon = mc.connector.connect( host=hostname, user=username, passwd=root, db = bot_data)
-    mycursor = myCon.cursor()
-    sql = "INSERT INTO user_data (name, age, home, course) VALUES (%s, %s, %s, %s)"
-    val = (name, age, home, course)
-    mycursor.execute(sql, val)
-    myCon.commit()
-    print(mycursor.rowcount, "record inserted.")"""
-    
-    
-    
-
+myCon.close()    
+print("Connection closed successfully.")                             
